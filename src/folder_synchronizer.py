f06640ca22d076de5c74f651136810ebbebdf6f2
Vlad <rotariuvladimir@gmail.com>
2023-09-25 16:45:58 +0200

    Modularize sha256 hashing of content

--- src/folder_synchronizer.py
+++ src/folder_synchronizer.py
@@ -1,5 +1,4 @@
 import os
-import hashlib
 import shutil
 import asyncio
 
@@ -8,6 +7,8 @@ from input_processing_actions import (
     validate_folder_path,
     read_validate_synchronization_interval,
 )
+
+from hashing_content import hash_folder_files
 
 print("Welcome to the Folder Synchronizer app!", "\n")
 
@@ -26,50 +27,6 @@ validate_folder_path(os_independent_path_replica_folder)
 os_independent_path_log_file = read_path(prompt_log_file)
 
 synchronization_interval: float = read_validate_synchronization_interval()
-
-
-def hash_file(file_name: str) -> str:
-    sha256_handler = hashlib.sha256()
-
-    with open(file_name, "rb") as file:
-        while True:
-            half_a_megabyte_batch: bytes = file.read(2**19)
-
-            if not half_a_megabyte_batch:
-                break
-
-            sha256_handler.update(half_a_megabyte_batch)
-
-    hashed_binary_file = sha256_handler.hexdigest()
-
-    return hashed_binary_file
-
-
-def hash_folder_files(path_folder: str):
-    paths_hashes_files = {}
-    directory_paths = []
-
-    for directory_path, directory_names, file_names in os.walk(path_folder):
-        directory_paths.append(directory_path)
-
-        for directory_name in directory_names:
-            inner_directory_path = os.path.join(directory_path, directory_name)
-            directory_paths.append(inner_directory_path)
-
-        for file_name in file_names:
-            os.chdir(directory_path)
-
-            directory_name = os.path.dirname(path_folder)
-
-            trunked_directory_path = directory_path.replace(
-                f"{os.path.sep}{directory_name}", ""
-            )
-
-            file_path = os.path.join(trunked_directory_path, file_name)
-
-            paths_hashes_files[f"{file_path}"] = [hash_file(file_name)]
-
-    return [directory_paths, paths_hashes_files]
 
 
 async def project_source_into_replica_folder(
